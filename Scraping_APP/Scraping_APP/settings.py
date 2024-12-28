1f7b87299e7880c6f8c1d190b992a09c4422a1de
Nikhil Patil <87567207+Nikhil18Patil@users.noreply.github.com>
2024-12-28 12:52:24 +0530

    Update settings.py

--- Scraping_APP/Scraping_APP/settings.py
+++ Scraping_APP/Scraping_APP/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'app.middleware.CustomCorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
